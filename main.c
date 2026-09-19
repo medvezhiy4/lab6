8b7adcc8fd294f36678f7a86e0809238b45f8966
медвежий4 <mihailbelov932@gmail.com>
2026-09-19 14:14:17 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,40 @@
+/*
+Белов Михаил Александрович 
+ПИ 1-1
+Лаб 6
+*/
 
+
+#include <stdio.h>
+
+int main(void) {
+    int h;
+    int w;
+
+    printf("Введите высоту и ширину прямоугольника: \n");
+
+    if (scanf("%d %d", & h, & w) != 2) {
+        printf("Input error\n");
+        return 1;
+    }
+
+    if (h < 2 || h > 10 || w < 2 || w > 10) {
+        printf("Range error\n");
+        return 1;
+    }
+
+    for (int row = 1; row <= h; row++) {
+        for (int column = 1; column <= w; column++) {
+            if (row == 1 || row == h || column == 1 || column == w)
+            {
+                printf("*");
+            } else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
